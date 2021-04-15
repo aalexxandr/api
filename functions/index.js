363eaaf2f14139dd4cd5b3366321bbc2d1a8d52c
alexandr <alexxaandr.m@gmail.com>
2021-04-15 12:23:57 +0500

    created GET POST PUT DETELE api functions

--- functions/index.js
+++ functions/index.js
@@ -1,27 +1,61 @@
-const functions = require("firebase-functions")
-const express = require('express')
-const cors = require('cors')
+const functions = require("firebase-functions");
+const express = require("express");
 
-const admin = require('firebase-admin')
-admin.initializeApp()
+const app = express();
 
-const app = express()
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-app.get('/', (req, res) => {
+// GET query without parameters
+app.get("/", async (req, res) => {
+  const snapshot = await admin.firestore().collection("users").get();
 
-})
+  const users = [];
+  snapshot.forEach((doc) => {
+    const id = doc.id;
+    const data = doc.data();
 
-app.poat('/', (req, res) => {
-  const user = req.body
+    users.push({id, ...data});
+  });
 
-  await admin.firestore().collection('users').add(user)
-
-  res.status(201).send()
-})
-
-exports.user = functions.https.onRequest(app)
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  res.status(200).send(JSON.stringify(users));
 });
+
+// GET query with parameters
+app.get("/:id", async (req, res) => {
+  const collection = await admin.firestore().collection("users");
+  const snapshot = await collection.doc(req.params.id).get();
+
+  const docId = snapshot.id;
+  const docData = snapshot.data();
+
+  res.status(200).send(JSON.stringify({id: docId, ...docData}));
+});
+
+// POST query
+app.post("/", async (req, res) => {
+  const users = req.body;
+
+  await admin.firestore().collection("users").add(users);
+  res.status(201).send();
+});
+
+// PUT query
+app.put("/:id", async (req, res) => {
+  const body = req.body;
+
+  const collection = await admin.firestore().collection("users");
+  await collection.doc(req.params.id).update({...body});
+
+  res.status(200).send();
+});
+
+// DELETE query
+app.delete("/:id", async (req, res) => {
+  const collection = await admin.firestore().collection("users");
+  await collection.doc(req.params.id).delete();
+
+  res.status(200).send();
+});
+
+exports.user = functions.https.onRequest(app);
