baa67b0fe2b4bf664c79054374dfc0ba963bd7ab
alexandr <alexxaandr.m@gmail.com>
2021-06-05 21:00:08 +0500

    shit

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+// requests Rest API
 const functions = require("firebase-functions");
 const express = require("express");
 
@@ -24,20 +25,20 @@ app.get("/", async (req, res) => {
 });
 
 // GET query with parameters
-app.get("/:id", async (req, res) => {
-  const collection = await admin.firestore().collection(collectionName);
-  const snapshot = await collection.doc(req.params.id).get();
 
-  const docId = snapshot.id;
-  const docData = snapshot.data();
+// app.get("/:id", async (req, res) => {
+//   const collection = await admin.firestore().collection(collectionName);
+//   const snapshot = await collection.doc(req.params.id).get();
 
-  res.status(200).send(JSON.stringify({id: docId, ...docData}));
-});
+//   const docId = snapshot.id;
+//   const docData = snapshot.data();
+
+//   res.status(200).send(JSON.stringify({id: docId, ...docData}));
+// });
 
 // POST query
 app.post("/", async (req, res) => {
   const users = req.body;
-
   await admin.firestore().collection(collectionName).add(users);
   res.status(201).send();
 });
@@ -60,4 +61,52 @@ app.delete("/:id", async (req, res) => {
   res.status(200).send();
 });
 
-exports.user = functions.https.onRequest(app);
+exports.requests = functions.https.onRequest(app);
+
+// //users Rest API
+
+// const appUsers = express();
+
+// // GET query without parameters
+// appUsers.get("/", async (req, res) => {
+//     const snapshot = await admin.firestore().collection(collectionName).get();
+  
+//     const users = [];
+//     snapshot.forEach((doc) => {
+//       const id = doc.id;
+//       const data = doc.data();
+  
+//       users.push({id, ...data});
+//     });
+  
+//     res.status(200).send(JSON.stringify(users));
+//   });
+  
+  // GET query with parameters
+
+//   appUsers.get("/:id", async (req, res) => {
+//     const collection = await admin.firestore().collection(collectionName);
+//     const snapshot = await collection.doc(req.params.id).get();
+  
+//     const docId = snapshot.id;
+//     const docData = snapshot.data();
+  
+//     res.status(200).send(JSON.stringify({id: docId, ...docData}));
+//   });
+  
+  // POST query
+//   appUsers.post("/", async (req, res) => {
+//     const users = req.body;
+//     await admin.firestore().collection(collectionName).add(users);
+//     res.status(201).send();
+//   });
+  
+//   // DELETE query
+//   appUsers.delete("/:id", async (req, res) => {
+//     const collection = await admin.firestore().collection(collectionName);
+//     await collection.doc(req.params.id).delete();
+  
+//     res.status(200).send();
+//   });
+  
+// exports.users = functions.https.onRequest(appUsers);
